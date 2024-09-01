8d16315202e287527dfd052a752c63a0883d9a7a
venkatk-git <venkatkumar.profiles@gmail.com>
2024-09-01 11:28:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="">1GHzCompiler</div>;
+}
+
+export default App;
---
+++ src/features/compiler/Compiler.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Compiler() {
+  return <div>Compiler</div>;
+}
+
+export default Compiler;
